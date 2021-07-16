12c74cbdcaa306e1b435576f47f74e59964ce48a
Nasrinbanu1830 <nasrinsyed18@gmail.com>
2021-07-16 18:13:35 +0530

    card created

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+const taskContainer = document.querySelector(".task__container");
+console.log(taskContainer);
+const newCard =({id,imageUrl,taskTitle,taskDescription,taskType}) => `<div class="col-md-6 col-lg-4" id=${id}>
+<div class="card">
+  <div class="card-header d-flex justify-content-end gap-2">
+    <button type="button" class="btn btn-outline-success"><i class="fas fa-pencil-alt"></i></button>
+    <button type="button" class="btn btn-outline-danger"><i class="fas fa-trash-alt"></i></button>
+
+  </div>
+  <img src=${imageUrl}
+  class="card-img-top rounded" alt="image">
+  <div class="card-body">
+    <h5 class="card-title">${taskTitle}</h5>
+    <p class="card-text">${taskDescription}</p>
+    <span class="badge bg-primary">${taskType}</span>
+  </div>
+  <div class="card-footer text-muted">
+    <button type="button" class="btn btn-outline-primary float-end">Open Task</button>
+
+  </div>
+</div>
+</div>`;
+
+
+
 const saveChanges = () => {
     const taskData = {
-        id:'${Date.now()}',
-        imageUrl: document.getElementById("imageUrl"),
-        taskTitle: document.getElementById("tasktitle"),
-        tastType: document.getElementById("tasktype"),
-        taskDescription: document.getElementById("taskdescription"),
+        id:`${Date.now()}`,  //unique number for card id     
+        imageUrl: document.getElementById("imageurl").value,
+        taskTitle: document.getElementById("tasktitle").value,
+        taskDescription: document.getElementById("taskdescription").value,
+        taskType: document.getElementById("tasktype").value,
+
     };
-    console.log(taskData);
+    const createNewCard = newcard(taskData);
+    taskContainer.insertAdjacentHTML("beforeend",createNewCard);
 };
